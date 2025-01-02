ac527fe3bca5aa7237633502c59c3379f92a445e
zainzaka0786 <zainzaka265@gmail.com>
2025-01-02 14:03:02 +0500

    some changes

--- src/components/CustomButtom.js
+++ src/components/CustomButtom.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { THEME_COLOR , WHITE_COLOR} from "../res/colors";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
-
-const CustomButton = (props) => {
-  const { title, onPress, style, textStyle } =props
+const CustomButton = ({ title, onPress, backgroundColor, borderColor , style, textStyle , }) => {
   return (
     <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
       <Text style={[styles.buttonText, textStyle]}>{title}</Text>
@@ -13,16 +11,15 @@ const CustomButton = (props) => {
 const styles = StyleSheet.create({
   button: {
     backgroundColor: THEME_COLOR,
-    padding: 15,
-    borderRadius: 25,
-    marginVertical: 20,
-    // width: "100%",
-    fontWeight: "bold",
-    fontSize: 16,
+    padding: 10,
+    borderRadius: 37.5,
+    marginVertical: 5,
+    borderColor:THEME_COLOR,
+    borderWidth:2,
+    width: "100%",
     alignItems: "center",
   },
   buttonText: {
-    color: WHITE_COLOR,
     fontWeight: "bold",
     fontSize: 24,
   },
